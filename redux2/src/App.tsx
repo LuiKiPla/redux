074144f5d55a3b6609773d47c288b201f1e27e74
LuiKiPla <113294704+LuiKiPla@users.noreply.github.com>
2025-09-04 13:04:55 +0000

    finish product apo with error

--- redux2/src/App.tsx
+++ redux2/src/App.tsx
@@ -1,12 +1,40 @@
 import './App.css'
+import { useAppDispatch, useAppSelector} from './store/products/hooks'
+import { useEffect } from 'react'
+import { fetchProducts } from './store/products/actionCreators'
+import type { product } from './store/products/types'
 
+
+        
 function App() {
 
+  const { products, loading, error } = useAppSelector(state=> state.productsSlice)
+  const dispatch = useAppDispatch()
+
+  useEffect(()=> {
+    dispatch(fetchProducts())
+  })
+
+if (loading) {
+    return <h1>Идёт загрузка!</h1>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+  
   return (
-    <>
-      
-    </>
-  )
+  <>
+    <div>
+      {products.map((pro: product) => (
+        <div key={pro.id}>
+          <img src={pro.image} alt ={pro.title} />
+          {pro.title}
+        </div>
+      ))}
+    </div>
+  </>
+)
 }
 
 export default App
--- redux2/src/store/products/reducer.ts
+++ redux2/src/store/products/reducer.ts
@@ -1,5 +1,5 @@
-import { combineReducers, createSlice } from "@reduxjs/toolkit";
-import type { productsState } from "./types";
+import { combineReducers, createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { productsState, product } from "./types";
 import { fetchProducts } from './actionCreators'
 
 const initialState: productsState = {
@@ -18,7 +18,15 @@ const productsSlice = createSlice({
             .addCase(fetchProducts.pending, (state) =>{
                 state.loading = true
             })
-    }
+.addCase(fetchProducts.fulfilled,(state,action: PayloadAction<product[]>) => {
+    state.loading = false
+    state.products = action.payload
+    })
+.addCase(fetchProducts.rejected,(state,action ) => {
+            state.loading = false
+            state.error = action.payload as string 
+            })
+         }
 })
 
 const rootReducer = combineReducers({
